873dfa9cf2b1e5821a116ac3a816271e4f86b508
Dyaoss <eogus12003@naver.com>
2024-06-04 21:42:00 +0900

    출력문구 수정

--- app/src/main/java/com/example/mycal/MainCal.kt
+++ app/src/main/java/com/example/mycal/MainCal.kt
@@ -2,28 +2,28 @@ package com.example.mycal
 
 fun main() {
     var cal = Calculator()
-    cal.mulNum()
+    cal.addNum()
 }
 
 class Calculator {
     fun addNum(){
         var num1 = readln().toInt()
         var num2 = readln().toInt()
-        println("결과: ${num1 + num2}")
+        println("$num1 + $num2 = ${num1 + num2}")
     }
     fun subNum(){
         var num1 = readln().toInt()
         var num2 = readln().toInt()
-        println("결과: ${num1 - num2}")
+        println("$num1 - $num2 = ${num1 + num2}")
     }
     fun mulNum(){
         var num1 = readln().toInt()
         var num2 = readln().toInt()
-        println("결과: ${num1 * num2}")
+        println("$num1 x $num2 = ${num1 + num2}")
     }
     fun divNum(){
         var num1 = readln().toInt()
         var num2 = readln().toInt()
-        println("결과: ${num1 / num2}")
+        println("$num1 / $num2 = ${num1 + num2}")
     }
 }
