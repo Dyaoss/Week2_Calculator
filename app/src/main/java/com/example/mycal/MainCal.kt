0fbb6175cc2006a88870f51ec89112da356c66c8
Dyaoss <eogus12003@naver.com>
2024-06-05 16:18:33 +0900

    1단계 - 연산자를 입력받아 연산하는 계산기

--- app/src/main/java/com/example/mycal/MainCal.kt
+++ app/src/main/java/com/example/mycal/MainCal.kt
@@ -2,28 +2,49 @@ package com.example.mycal
 
 fun main() {
     var cal = Calculator()
-    cal.mulNum()
+    cal.calculator()
 }
 
 class Calculator {
-    fun addNum(){
-        var num1 = readln().toInt()
-        var num2 = readln().toInt()
-        println("$num1 + $num2 = ${num1 + num2}")
+    var num1: Double = 0.0
+    var num2: Double = 0.0
+
+
+    init {
+        println("계산기 시작!")
     }
-    fun subNum(){
-        var num1 = readln().toInt()
-        var num2 = readln().toInt()
-        println("$num1 - $num2 = ${num1 - num2}")
+
+    fun calculator() {
+        var op: String
+
+        println("숫자를 입력해주세요.")
+        var num1 = readln().toDouble()
+        var num2 = readln().toDouble()
+        println("연산자를 입력해주세요.(+,-,*,/)")
+        op = readln()
+        when (op) {
+            "" -> println("연산자를 입력해주세요!")
+            "+" -> addNum(num1,num2)
+            "-" -> subNum(num1,num2)
+            "*" -> mulNum(num1,num2)
+            "/" -> divNum(num1,num2)
+            else -> println("연산자를 제대로 입력해주세요(+,-,*,/)")
+        }
     }
-    fun mulNum(){
-        var num1 = readln().toInt()
-        var num2 = readln().toInt()
-        println("$num1 x $num2 = ${num1 * num2}")
+
+    fun addNum(_num1:Double,_num2:Double) {
+        println("$_num1 + $_num2 = ${_num1 + num2}")
     }
-    fun divNum(){
-        var num1 = readln().toInt()
-        var num2 = readln().toInt()
-        println("$num1 / $num2 = ${num1 / num2}")
+
+    fun subNum(_num1:Double,_num2:Double) {
+        println("$_num1 - $_num2 = ${_num1 - _num2}")
+    }
+
+    fun mulNum(_num1:Double,_num2:Double) {
+        println("$_num1 x $_num2 = ${_num1 * _num2}")
+    }
+
+    fun divNum(_num1:Double,_num2:Double) {
+        println("$_num1 / $_num2 = ${_num1 / _num2}")
     }
 }
